4d41109690df3de670624353dea26fed26eb7d53
Platon <hypek60@gmail.com>
2023-02-03 09:19:41 +0500

    Добавьте файлы проекта.

---
+++ SumbitFor16/Models/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumbitFor16.Models
+{
+    public class Core
+    {
+        public AUTOSERVICEEntities context = new AUTOSERVICEEntities();
+    }
+}
