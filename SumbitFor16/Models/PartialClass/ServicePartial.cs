d84ca2301b20d98fa08812c9eaded277faf538cd
Platon <hypek60@gmail.com>
2023-02-06 13:28:33 +0500

    короче сделал

---
+++ SumbitFor16/Models/PartialClass/ServicePartial.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SumbitFor16.Models
+{
+    public partial class ServicePhoto 
+    {
+        public string MainImage
+        { get 
+            {
+                return "..\\..\\Assets\\Images\\" + PhotoPath;
+            } }
+    }
+
+    public partial class Service
+    {
+        public string DiscountText
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return "";
+                else
+                    return $"* скидка {Discount * 100}%";
+            }
+        }
+
+        public double CostWithDiscount 
+        {
+            get 
+            {
+                if (Discount == 0 || Discount == null)
+                {
+                    return (double)Cost;
+                }
+                else 
+                {
+                    var costWithDiscount = (double)Cost * (1.00 - Discount);
+                    return costWithDiscount.Value;
+                }
+            }
+        }
+
+        public string TotalCost
+        {
+            get
+            {
+                string[] vs = DurationInSeconds.Split(new char[] { ' ' });
+                if (vs[1] == "мин.") 
+                {
+                    int minut = Convert.ToInt32(vs[0]) * 60;
+                }
+                if (Discount == 0 || Discount == null)
+                {
+                    return $"{Cost:N2} рублей за {} минут";
+                }
+                else
+                {
+                    return $"{CostWithDiscount:N2} рублей за {} минут";
+                }
+            }
+        }
+
+        public Visibility DiscountVisibility 
+        {
+            get 
+            {
+                if (Discount == 0 || Discount == null)
+                {
+                    return Visibility.Collapsed;
+                }
+                else
+                {
+                    return Visibility.Visible;
+                }
+            }
+        }
+
+        public string BackColor 
+        {
+            get 
+            {
+                if (Discount == 0 || Discount == null)
+                {
+                    return "#FFFFE1";
+                }
+                else
+                {
+                    return "#D1FFD1";
+                }
+            }
+        }
+    }
+}
--- SumbitFor16/View/Pages/ServicePage.xaml.cs
+++ SumbitFor16/View/Pages/ServicePage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SumbitFor16.Models;
 
 namespace SumbitFor16.View.Pages
 {
@@ -20,12 +21,27 @@ namespace SumbitFor16.View.Pages
     /// </summary>
     public partial class ServicePage : Page
     {
+        Core db = new Core();
+        List<Service> arrayService;
+        
         public ServicePage()
         {
             InitializeComponent();
+            arrayService = db.context.Service.ToList();
+            LViewServices.ItemsSource = arrayService; 
         }
 
         private void BtnAddServiceClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEditClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDeleteClick(object sender, RoutedEventArgs e)
         {
 
         }
